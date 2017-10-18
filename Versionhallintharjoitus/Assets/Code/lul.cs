4f4a624c5367dc841a844ff9ee470c618cd9ea61
Mutti88 <juomakuski@hotmail.com>
2017-10-18 11:23:18 +0300

    Lul

---
+++ Versionhallintharjoitus/Assets/Code/lul.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lul : MonoBehaviour {
+    public float speed = 5;
+    void Update()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        Move(new Vector3(horizontal,0,vertical));
+    }
+
+    void Move(Vector3 Input)
+    {
+        float speed = 5;
+        Vector3 moventVector = Input * speed;
+        transform.Translate( moventVector*Time.deltaTime);
+    }
+}
